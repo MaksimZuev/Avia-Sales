ac95d4f89fbb8e77fb103920909412668aa17a07
Maksim Zuev <m-zuev@bk.ru>
2022-02-24 15:38:01 +0300

    avia-sales-v2

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer color="primary lighten-1" padless>
+    <v-row justify="center" no-gutters>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        Зуев Максим Тестовое задание
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+
+<script>
+export default {
+  name: "v-Footer",
+  data: () => ({}),
+};
+</script>
